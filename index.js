6c5b96ce2e768d88c4242e632c3845e6b8770be9
Jörgen Kristofer Rebane <jkrebane@users.noreply.github.com>
2023-10-12 17:11:07 +0300

    Tund 6 lõppenud

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const timeInfo = require('./datetime_fnc');
+const fs = require('fs');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
-    //res.send('See töötab!');
-    //res.download('index.js');
     res.render('index');
 });
 
-app.get('/test', (req,res)=>{
-    res.send('Test completed sucessfully!');
-    //res.download('index.js');
+app.get('/timenow', (req,res)=>{
+    const dateNow = timeInfo.dateETformatted();
+    const timeNow = timeInfo.timeETformatted();
+    res.render('timenow', {nowD: dateNow, nowT: timeNow});
+});
+
+app.get('/wisdom', (req,res)=>{
+    let folkwisdom = [];
+    fs.readFile('public/txtfiles/vanasonad.txt', 'utf8', (err, data)=>{
+        if(err) {
+            throw err;
+        }
+        else {
+            folkwisdom = data.split(';');
+            res.render('justlist', {h1: 'Vanasõnad', wisdom: folkwisdom});
+        }
+    });
 });
 
 app.listen(5125);
